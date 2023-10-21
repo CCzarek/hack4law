4db445fed025f4382fa8f6ce3eeef8c5c02f4e4c
kseligga <57732056+kseligga@users.noreply.github.com>
2023-10-21 19:02:31 +0200

    cleaning textContent field

--- df_parsing.py
+++ df_parsing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import datetime
+import re
+
 
 pd.set_option('display.max_columns', None)
 df = pd.read_csv('2023_orzeczenia.csv')
@@ -18,9 +20,6 @@ df.drop(columns=['Unnamed: 0', 'decision', 'receiptDate','meansOfAppeal', 'judgm
 df.info()
 
 
-format = '%Y-%m-%d'
-input = '2021/05/25'
-
 # convert from string format to datetime format
 
 #print(datetime.datetime.strptime(df['judgmentDate'], format))
@@ -30,13 +29,26 @@ for (_, orzeczenie) in df.iterrows():
     if tmp_data>max_data:
         max_data=tmp_data
 #print(max(datatime(df['judgmentDate']))
-print(max_data)
+print(max_data) # last date
 
 # sprawdzanie czy dziala (czy bez duplikatow)
-print(df.duplicated())
-
 print(sum(df.duplicated()))
 
-print(df.iloc[0]==df.iloc[100])
+# deleting data from 'future'
+for (_, orzeczenie) in df.iterrows():
+    if datetime.datetime.today() < datetime.datetime.strptime(orzeczenie['judgmentDate'], format):
+        df.drop(_, inplace=True)
 
-print(df['summary'].values[100], print(df['summary'].values[200]))
+
+# cleaning textContent
+CLEANR = re.compile('<.*?>')
+def cleanhtml(raw_html):
+  cleantext = re.sub(CLEANR, '', raw_html)
+
+  return cleantext.strip()
+
+#print(df['textContent'][5839])
+df['textContent'] = df['textContent'].apply(lambda x: cleanhtml(str(x)))
+df['textContent'] = df['textContent'].apply(lambda x: x.replace('\n', ' ').replace('\r', ''))
+
+print(df['textContent'])
