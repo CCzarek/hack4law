8c955cc64a4ed30dd7c41e036bea461739941519
kseligga <57732056+kseligga@users.noreply.github.com>
2023-10-21 18:11:40 +0200

    fixed getting data from api

--- df_parsing.py
+++ df_parsing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 pd.set_option('display.max_columns', None)
 df = pd.read_csv('2023_orzeczenia.csv')
@@ -16,4 +17,26 @@ df.drop(columns=['Unnamed: 0', 'decision', 'receiptDate','meansOfAppeal', 'judgm
 
 df.info()
 
-print((df['judgmentDate']))
+
+format = '%Y-%m-%d'
+input = '2021/05/25'
+
+# convert from string format to datetime format
+
+#print(datetime.datetime.strptime(df['judgmentDate'], format))
+max_data=datetime.datetime.strptime('1970-01-01', format)
+for (_, orzeczenie) in df.iterrows():
+    tmp_data = datetime.datetime.strptime(orzeczenie['judgmentDate'], format)
+    if tmp_data>max_data:
+        max_data=tmp_data
+#print(max(datatime(df['judgmentDate']))
+print(max_data)
+
+# sprawdzanie czy nie ma duplikatow
+print(df.duplicated())
+
+print(sum(df.duplicated()))
+
+print(df.iloc[0]==df.iloc[100])
+
+print(df['summary'].values[100], print(df['summary'].values[200]))
